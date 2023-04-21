9c5ac72b24a37cb1b7c71c2b9426b30582836d0d
songjisuk <jisuksong@naver.com>
2023-04-21 17:57:08 +0900

    init

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+class FlowEx6{
+    public static void main(String[] args){
+        System.out.print("현재 월을 입력하세요.>");
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        int month = scanner.nextInt();
+
+        switch (month){
+            case 3: case 4: case 5:
+                System.out.println("현재의 계절은 봄입니다.");
+                break;
+
+            case 6: case 7: case 8:
+                System.out.println("현재의 계절은 여름입니다.");
+                break;
+
+            case 9: case 10: case 11:
+                System.out.println("현재의 계절은 가을입니다.");
+                break;
+
+            default:
+            case 12: case 1: case 2:
+                System.out.println("현재의 계절은 겨울입니다.");
+                break;
+        }
     }
 }
